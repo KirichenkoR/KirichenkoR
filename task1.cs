732c00a663bd95f07ca38b98cf8e3858c57ebbce
KirichenkoR <105743179+KirichenkoR@users.noreply.github.com>
2022-05-17 23:22:06 +0300

    Update task1.cs
    Финальная версия

--- task1.cs
+++ task1.cs
@@ -19,7 +19,34 @@ public class Program
 		Console.Write("Введите значения второго массива:\t");
 		for (int j = 0; j < elementsCount1; j++)
            {
+<<<<<<< Updated upstream
                a1[j]=Convert.ToInt32(Console.ReadLine());
            }
+=======
+               a2[j]=Convert.ToInt32(Console.ReadLine());
+           }
+        
+        int a = 0;
+        Console.Write("Введите позицию начала замены:\t");
+        a=Convert.ToInt32(Console.ReadLine());
+
+        Array.Resize(ref a1, elementsCount1+a-1);
+
+        int k = 0;
+        for (int i = a-1; i < a1.Length; i++)
+           {
+               a1[i] = a2[k];
+               k++;
+           }
+
+        Console.Write("Значения первого массива после слияния:\t");
+
+        for (int i = 0; i < a1.Length; i++)
+           {
+		      Console.WriteLine(a1[i]);
+           }
+
+
+>>>>>>> Stashed changes
 	}
 }
