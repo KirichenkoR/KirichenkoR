b72527d07626b0cc91b3de964d6fffa9de4a2197
KirichenkoR <105743179+KirichenkoR@users.noreply.github.com>
2022-05-17 23:03:02 +0300

    Update task1.cs
    скрещиваем массивы

--- task1.cs
+++ task1.cs
@@ -6,20 +6,49 @@ public class Program
 	{
 		Console.Write("Введите количество элементов первого массива:\t");
 		int elementsCount1 = Convert.ToInt32(Console.ReadLine());
-		int[] a1 = new int[elementsCount1];
-		Console.Write("Введите количество элементов второго массива:\t");
+
+        Console.Write("Введите количество элементов второго массива:\t");
 		int elementsCount2 = Convert.ToInt32(Console.ReadLine());
+
+        int[] a1 = new int[elementsCount1];
+		
 		int[] a2 = new int[elementsCount2];
-		int s=0;
+		
+        int s = 0;
 		Console.Write("Введите значения первого массива:\t");
+
 		for (int i = 0; i < elementsCount1; i++)
            {
                a1[i]=Convert.ToInt32(Console.ReadLine());
            }
+
 		Console.Write("Введите значения второго массива:\t");
+
 		for (int j = 0; j < elementsCount1; j++)
            {
                a1[j]=Convert.ToInt32(Console.ReadLine());
            }
+        
+        int a = 0;
+        Console.Write("Введите позицию начала замены:\t");
+        a=Convert.ToInt32(Console.ReadLine());
+
+        Array.Resize(ref a1, elementsCount1+a);
+
+        for (int i = a; i < a1.Length; i++)
+           {
+               int j = 0;
+               a1[i] = a2[j];
+               j++;
+           }
+
+        Console.Write("Значения первого массива:\t");
+
+        for (int i = 0; i < a1.Length; i++)
+           {
+		      Console.WriteLine(a1[i]);
+           }
+
+
 	}
 }
